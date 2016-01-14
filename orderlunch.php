bb70f30701764da2277c386e08319d2e592f609f
BrainyBrian <dgramopadhye@gmail.com>
2016-01-14 15:52:55 -0500

    Create orderlunch.php

---
+++ orderlunch.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<form action="step2.php" method="POST">
+			ID:<input type="password" name="studentid"><br/>
+			Number:<input type="password" name="lunchnumber"><br/>
+			<input type="submit" >
+		</form>
+	</body>
+</html>
